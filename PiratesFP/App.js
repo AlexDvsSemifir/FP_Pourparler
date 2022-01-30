4a1310674aafbbc158fe42e54ce7dd49f3dbb128
Oh_My_Goat <daftouney@gmail.com>
2022-01-30 12:44:55 +0100

    feat(App.js): Player Selection works

--- PiratesFP/App.js
+++ PiratesFP/App.js
@@ -2,14 +2,28 @@
 
 import { StatusBar } from "expo-status-bar";
 import React, { useState, useEffect, Component } from "react";
+import {
+  Button,
+  Form,
+  FormGroup,
+  FormLabel,
+  FormSelect,
+} from "react-bootstrap";
 import { Text, View } from "react-native";
-
-// API data :
 
 // App
 
 export default function App() {
-  
+
+  let [playerSelection, setPlayerSelection] = useState(1);
+
+  const [playerList, setPlayerList] = useState([
+    {
+      id: "",
+      bio: "",
+    },
+  ]);
+
   const [player, setPlayer] = useState({
     id: "",
     bio: "",
@@ -19,15 +33,15 @@ export default function App() {
     money: "",
     inventory: {
       loot: "",
-      cloth: ""
+      cloth: "",
     },
     weapons: {
       slot1: "",
       slot2: "",
       slot3: "",
       slot4: "",
-      slot5: ""
-    }
+      slot5: "",
+    },
   });
 
   const getPlayer = async (id) => {
@@ -35,20 +49,55 @@ export default function App() {
       const response = await fetch("http://localhost:8080/players/" + id);
       const json = await response.json();
       console.log(json);
-      setPlayer((json));
+      setPlayer(json);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const getPlayerList = async () => {
+    try {
+      const response = await fetch("http://localhost:8080/players/");
+      const json = await response.json();
+      console.log(json);
+      setPlayerList(json);
     } catch (error) {
       console.log(error);
     }
   };
 
   useEffect(() => {
-    getPlayer(1);
+    getPlayerList();
+    getPlayer(playerSelection);
   }, []);
+
+  
+  const handlePlayerSelection = (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+    setPlayerSelection(e.target.value)
+    getPlayer(playerSelection)
+  }
 
   return (
     <View>
-      <Text>On tente des trucs !</Text>
-      <Text>{player.id}</Text>
+      <Form onSubmit={handlePlayerSelection}>
+        <FormGroup className="mb-3">
+          <FormSelect aria-label="Personnage" onChange={handlePlayerSelection}>
+            <option>Sélectionnez votre joueur :</option>
+            {playerList.map((player) => {
+              return (
+                <option key={player.id} value={player.id} >
+                  {player.bio.firstName} 
+                </option>
+              );
+            })}
+          </FormSelect>
+          <Button type="submit">Pick</Button>
+        </FormGroup>
+      </Form>
+      <Text>Player ID : {player.id}</Text>
+      <Text>Player Name : {player.bio.firstName}</Text>
       <StatusBar style="auto" />
     </View>
   );
